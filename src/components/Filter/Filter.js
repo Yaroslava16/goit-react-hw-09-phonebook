7e0f612c1b9b4b59f55fdd3c6fb3b0725727d316
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-07-31 00:54:19 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,33 +1,27 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './Filter.module.css';
-import { connect } from 'react-redux';
 import { filter } from '../../redux/phonebook';
 import { getFilter } from '../../redux/phonebook';
 
-const Filter = ({ value, onChange }) => (
-  <label className={styles.label}>
-    Find contacts by name
-    <input
-      className={styles.input}
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-  </label>
-);
+export default function Filter() {
+  const dispatch = useDispatch();
 
-// Filter.propTypes = {
-//   filter: PropTypes.string.isRequired,
-//   filterContacts: PropTypes.func.isRequired,
-// };
+  const value = useSelector(getFilter);
+  const onChange = useCallback(
+    e => dispatch(filter(e.target.value)),
+    [dispatch],
+  );
 
-const mapStateToProps = state => ({
-  value: getFilter(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  onChange: e => dispatch(filter(e.target.value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Filter);
+  return (
+    <label className={styles.label}>
+      Find contacts by name
+      <input
+        className={styles.input}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
