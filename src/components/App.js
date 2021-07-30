0b1d8f1b468e5002a487578bddfaae062a5644b5
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-07-30 16:06:20 +0300

    Change mapDispatchToProps to useDispatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,49 @@
-import { Component, Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { getCurrentUser } from '../redux/auth';
 import Container from './Container/Conteiner';
 import AppBar from './AppBar';
 
 import PrivatRoute from './PrivatRoute';
 import PublicRoute from './PublicRoute';
+import { useDispatch } from 'react-redux';
 
 const HomeView = lazy(() => import('../views/HomeView'));
 const RegisterView = lazy(() => import('../views/RegisterView'));
 const LoginView = lazy(() => import('../views/LoginView'));
 const ContactsView = lazy(() => import('../views/ContactsView'));
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onGetCurrentUser();
-  }
+export default function App() {
+  const dispatch = useDispatch();
 
-  render() {
-    return (
-      <Container>
-        <AppBar />
-        <Suspense fallback={<p>Loading...</p>}>
-          <Switch>
-            <PublicRoute exact path="/" component={HomeView} />
-            <PublicRoute
-              path="/register"
-              restricted
-              redirectTo="/contacts"
-              component={RegisterView}
-            />
-            <PublicRoute
-              path="/login"
-              restricted
-              redirectTo="/contacts"
-              component={LoginView}
-            />
-            <PrivatRoute
-              path="/contacts"
-              redirectTo="/login"
-              component={ContactsView}
-            />
-          </Switch>
-        </Suspense>
-      </Container>
-    );
-  }
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+  return (
+    <Container>
+      <AppBar />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <PublicRoute exact path="/" component={HomeView} />
+          <PublicRoute
+            path="/register"
+            restricted
+            redirectTo="/contacts"
+            component={RegisterView}
+          />
+          <PublicRoute
+            path="/login"
+            restricted
+            redirectTo="/contacts"
+            component={LoginView}
+          />
+          <PrivatRoute
+            path="/contacts"
+            redirectTo="/login"
+            component={ContactsView}
+          />
+        </Switch>
+      </Suspense>
+    </Container>
+  );
 }
-
-const mapDispatchToProps = {
-  onGetCurrentUser: getCurrentUser,
-};
-
-export default connect(null, mapDispatchToProps)(App);
--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,3 +1,4 @@
+// import { useState } from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { register } from '../redux/auth/auth-operations';
@@ -12,6 +13,55 @@ const styles = {
     marginBottom: 15,
   },
 };
+
+// export default function RegisterView() {
+//   const [name, setName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   return (
+//     <div>
+//       <h1>Registration page</h1>
+
+//       <form autoComplete="off" onSubmit={handleSubmit} style={styles.form}>
+//         <label style={styles.label}>
+//           Name
+//           <input
+//             autoComplete="off"
+//             type="text"
+//             name="name"
+//             value={name}
+//             onChange={handleChange}
+//           />
+//         </label>
+
+//         <label style={styles.label}>
+//           Email
+//           <input
+//             autoComplete="off"
+//             type="email"
+//             name="email"
+//             value={email}
+//             onChange={handleChange}
+//           />
+//         </label>
+
+//         <label style={styles.label}>
+//           Password
+//           <input
+//             autoComplete="off"
+//             type="password"
+//             name="password"
+//             value={password}
+//             onChange={handleChange}
+//           />
+//         </label>
+
+//         <button type="submit">Register now</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 class RegisterView extends Component {
   state = {
@@ -34,50 +84,55 @@ class RegisterView extends Component {
     const { name, email, password } = this.state;
 
     return (
-      <div>
-        <h1>Registration page</h1>
+      <>
+        <div>
+          <h1>Registration page</h1>
 
-        <form
-          autoComplete="off"
-          onSubmit={this.handleSubmit}
-          style={styles.form}
-        >
-          <label style={styles.label}>
-            Name
-            <input
+          <form
             autoComplete="off"
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
+            onSubmit={this.handleSubmit}
+            style={styles.form}
+          >
+            <label style={styles.label}>
+              Name
+              <input
+                autoComplete="off"
+                type="text"
+                name="name"
+                value={name}
+                // eslint-disable-next-line no-undef
+                onChange={this.handleChange}
+              />
+            </label>
 
-          <label style={styles.label}>
-            Email
-            <input
-            autoComplete="off"
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
+            <label style={styles.label}>
+              Email
+              <input
+                autoComplete="off"
+                type="email"
+                name="email"
+                value={email}
+                // eslint-disable-next-line no-undef
+                onChange={this.handleChange}
+              />
+            </label>
 
-          <label style={styles.label}>
-            Password
-            <input
-            autoComplete="off"
-              type="password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </label>
+            <label style={styles.label}>
+              Password
+              <input
+                autoComplete="off"
+                type="password"
+                name="password"
+                value={password}
+                // eslint-disable-next-line no-undef
+                onChange={this.handleChange}
+              />
+            </label>
 
-          <button type="submit">Register now</button>
-        </form>
-      </div>
+            <button type="submit">Register now</button>
+          </form>
+        </div>
+      </>
     );
   }
 }
